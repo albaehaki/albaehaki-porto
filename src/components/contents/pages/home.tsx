78a221c056210300d39600cdc70392db766af625
alzdirty <zackyalbaehaki@gmail.com>
2022-03-05 01:15:35 +0700

    sun and moon toggles

--- src/components/contents/pages/home.tsx
+++ src/components/contents/pages/home.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className="grid h-screen">
       <h1 className="m-auto text-7xl">Zacky Al Baehaki</h1>
+      <h1 className="m-auto text-7xl">Web Developer</h1>
     </div>
   );
 };
