9f639d7969bc78434bed84716939f269f0aac38c
alzdirty <zackyalbaehaki@gmail.com>
2022-03-14 14:35:20 +0700

    baru

--- src/components/contents/pages/galeri.tsx
+++ src/components/contents/pages/galeri.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Page1 from "./galeri_root/page1";
+//import img from "../../../img/Albaehaki - Google Chrome 3_8_2022 6_41_18 AM.png";
 
 const galeri = () => {
   return (
     <div className="flex h-screen">
-      <p className="m-auto font-bold text-xl">COMING SOON</p>
+      <Routes>
+        <Route path="/galeri" element={<Page1 />} />
+      </Routes>
     </div>
   );
 };
