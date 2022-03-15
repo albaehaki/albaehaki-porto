e5a109092703b7d7dfa3aedcdc104d53a163d136
alzdirty <zackyalbaehaki@gmail.com>
2022-03-15 14:49:34 +0700

    galeri coming soon

--- src/components/contents/pages/galeri.tsx
+++ src/components/contents/pages/galeri.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Page1 from "./galeri_root/page1";
-//import Img from "../../../img/1.png";
 
 const galeri = () => {
   return (
     <div className="h-screen">
-      {/* <img src={img} /> */}
-      {/* <Routes>
-        <Route path="/galeri" element={<Page1 />} />
-      </Routes> */}
       <p>Zacky Al Baehaki</p>
     </div>
   );
