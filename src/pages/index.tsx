0b23218a56f865e7e6756f6d3178e20dd416d544
albaehaki <zackyalbaehaki@gmail.com>
2023-04-08 19:31:17 +0700

    portofolio baru

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+          test
             By{' '}
             <Image
               src="/vercel.svg"
