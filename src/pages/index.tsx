4b4592a921f20b03b2391c88cb212ecbc90a69df
albaehaki <zackyalbaehaki@gmail.com>
2023-04-08 19:42:00 +0700

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-          test
+          test lorem
             By{' '}
             <Image
               src="/vercel.svg"
